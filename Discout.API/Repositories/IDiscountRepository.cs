4aa0575d2f9e09871a08021e030acb5e55a429fd
Carlos Gimenes <contato.carlosgimenes@outlook.com>
2022-02-20 13:54:35 -0300

    Creating entities and repositories

---
+++ Discout.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discout.API.Entities;
+
+namespace Discout.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
