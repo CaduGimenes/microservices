295fde1f1156f6cacd94204fade6eefdf1968cf6
Carlos Gimenes <contato.carlosgimenes@outlook.com>
2022-02-20 12:54:54 -0300

    Adding Redis to project

--- Basket.API/Program.cs
+++ Basket.API/Program.cs
@@ -1,18 +1,17 @@
-using Catalog.API.Data;
-using Catalog.API.Repositories;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
 builder.Services.AddControllers();
-
-
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<ICatalogContext, CatalogContext>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
